c80a1c15b9cf0382f73e98a1532695ec7f8f58f5
bloodchen <bloodchen@gmail.com>
2021-12-22 11:28:15 +0800

    update

--- js/src/forum/util.js
+++ js/src/forum/util.js
@@ -1,38 +1,43 @@
 export default class UTIL {
     static decodeContent(str) {
         let ret = null;
-        
-        ret = UTIL.decrypt("textToChars(salt)",str);
-        if(ret.slice(0,2)!="||") 
-        	ret = str;
-        else
-            ret = ret.slice(2)
-        
-        if (ret.indexOf('http://') != -1 || ret.indexOf('https://') != -1) 
-            ret =`<a href='${ret}'>${ret}</a>`;
-        
+
+        ret = UTIL.b64DecodeUnicode(str);
+        if (ret.slice(0, 2) != '||') ret = str;
+        else ret = ret.slice(2);
+
+        if (ret.indexOf('http://') != -1 || ret.indexOf('https://') != -1) ret = `<a href='${ret}'>${ret}</a>`;
+
         return ret;
     }
-    static encodeContent(str){
-    	str = "||" + str;
-    	return UTIL.crypt("textToChars(salt)",str)
+    static encodeContent(str) {
+        str = '||' + str;
+        return UTIL.b64EncodeUnicode(str);
     }
-    static crypt(salt, text) {
-        const textToChars = (text) => text.split('').map((c) => c.charCodeAt(0));
-        const byteHex = (n) => ('0' + Number(n).toString(16)).substr(-2);
-        const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
-
-        return text.split('').map(textToChars).map(applySaltToChar).map(byteHex).join('');
+    static b64EncodeUnicode(str) {
+        // first we use encodeURIComponent to get percent-encoded UTF-8,
+        // then we convert the percent encodings into raw bytes which
+        // can be fed into btoa.
+        return btoa(
+            encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function toSolidBytes(match, p1) {
+                return String.fromCharCode('0x' + p1);
+            })
+        );
     }
-
-    static decrypt(salt, encoded) {
-        const textToChars = (text) => text.split('').map((c) => c.charCodeAt(0));
-        const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
-        return encoded
-            .match(/.{1,2}/g)
-            .map((hex) => parseInt(hex, 16))
-            .map(applySaltToChar)
-            .map((charCode) => String.fromCharCode(charCode))
-            .join('');
+    static b64DecodeUnicode(str) {
+        // Going backwards: from bytestream, to percent-encoding, to original string.
+        try{
+	        const ret = decodeURIComponent(
+	            atob(str)
+	                .split('')
+	                .map(function (c) {
+	                    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+	                })
+	                .join('')
+	        );
+	        return ret
+    	}catch(e){
+    		return str
+    	}
     }
 }
